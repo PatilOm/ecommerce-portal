904ce496824a4a2264e5eeb8cb1afae92465b741
PATIL <PATIL@DESKTOP-96AVB1I>
2019-01-10 14:12:56 +0530

    Till part 08-05 completed.

--- pro-frontend/src/main/java/com/omkar/pro_frontend/service/CartService.java
+++ pro-frontend/src/main/java/com/omkar/pro_frontend/service/CartService.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.omkar.pro.dao.CartLineDAO;
 import com.omkar.pro.dto.Cart;
 import com.omkar.pro.dto.CartLine;
+import com.omkar.pro.dto.Product;
 import com.omkar.pro_frontend.model.UserModel;
 
 @Service("cartService")
@@ -30,5 +31,35 @@ public class CartService {
 	public List<CartLine> getCartLines(){
 		Cart cart = this.getCart();
 		return cartLineDAO.list(cart.getId());
+	}
+
+	public String updateCartLine(int cartLineId, int count) {
+		
+		//fetch the cart line
+		CartLine cartLine = cartLineDAO.get(cartLineId);
+		
+		if(cartLine == null) {
+			return "result=error";
+		}
+		else {
+			Product product = cartLine.getProduct();
+			double oldTotal = cartLine.getTotal();
+			
+			if(product.getQuantity() <= count) {
+				count = product.getQuantity();
+			}
+			cartLine.setProductCount(count);
+			cartLine.setBuyingPrice(product.getUnitPrice());
+			cartLine.setTotal(product.getUnitPrice() * count);
+			
+			cartLineDAO.update(cartLine);
+			
+			Cart cart = this.getCart();
+			cart.setGrandTotal(cart.getGrandTotal() - oldTotal + cartLine.getTotal());
+			
+			cartLineDAO.updateCart(cart);
+			
+			return "result=updated";
+		}
 	}
 }
